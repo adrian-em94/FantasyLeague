b3a89c47cc7d1a6cb0df2e9724f538eaf0e75713
Adrian <adrianmartin2394@gmail.com>
2021-09-04 12:57:10 -0700

    added the buttons for players on pitch

--- frontend/src/pages/TeamProfile/TeamProfile.js
+++ frontend/src/pages/TeamProfile/TeamProfile.js
@@ -14,8 +14,23 @@ const TeamProfile = () => {
                                 <img src={Pitch} alt="soccer field"/>
                             </div>
                             <div className="players">
-                                <div className="goalie" />
-                                    
+                                <button className="goalie" type="button">1</button>
+
+                                
+                                <button className="leftBack" type="button">2</button>
+                                <button className="leftCenterBack" type="button">3</button>
+                                <button className="rightCenterBack" type="button">4</button>
+                                <button className="rightBack" type="button">5</button>
+
+                                <button className="leftMiddle" type="button">6</button>
+                                <button className="centerMiddle" type="button">10</button>
+                                <button className="rightMiddle" type="button">8</button>
+
+                                <button className="leftForward" type="button">9</button>
+                                <button className="centerForward" type="button">11</button>
+                                <button className="rightForward" type="button">7</button>
+
+
                                 
                             </div>
                         </div>
